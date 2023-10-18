854ddc6fcbae42a7420a32feeb7779cfb5a56ea9
zillurahman10 <zillur.hero40@gmail.com>
2023-10-18 12:11:20 +0600

    all problem done

--- D_Strings.c
+++ D_Strings.c
@@ -1,7 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
+    char A[100], B[100];
+    char C[200];
+    
+    // Input two strings A and B
+    scanf("%s", A);
+    
+    scanf("%s", B);
+    
+    // Calculate the sizes of A and B
+    int sizeA = strlen(A);
+    int sizeB = strlen(B);
+    
+    // Concatenate A and B
+    strcpy(C, A);
+    strcat(C, B);
+    
+    // Swap the first characters of A and B
+    char temp = A[0];
+    A[0] = B[0];
+    B[0] = temp;
+    
+    // Print the results
+    printf("%d %d\n", sizeA, sizeB);
+    printf("%s\n", C);
+    printf("%s %s\n", A, B);
     
     return 0;
 }
